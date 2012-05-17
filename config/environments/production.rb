7d5ec046597172ef0bc8f75b6d05faed8a1c08da
Kristján Pétursson <kristjan@gmail.com>
2012-05-17 10:40:40 -0700

    Rename Skeleton app to FoodieFinder

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-SinglyRailsSkeleton::Application.configure do
+FoodieFinder::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
